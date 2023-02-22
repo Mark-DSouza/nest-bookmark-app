b144c59f1c0a3fa8495a5c172030c5b9c4c3fe71
Mark-DSouza <markdsouza434@gmail.com>
2023-02-22 11:08:57 +0530

    Add global prisma module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { PrismaModule } from './../prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
